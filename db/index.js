9d184d749eee00196dc0118609cc12b6309d35d1
Sam Karp <sfcypret@gmail.com>
2022-09-11 16:12:43 -0400

    function index.js for all requests

---
+++ db/index.js
@@ -0,0 +1,32 @@
+const connection = require('./connection');
+
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+//department functions
+
+//view all 
+
+
+// add departments
+
+
+//role functions
+
+//view all roles
+
+
+// new roles
+
+// employee functions
+
+// view all employees
+
+//new employee
+
+//update employee
+}
+
+
+module.exports = new DB(connection);
